7feed97b379a9333a368fe2cce1b15da2e0d9adf
Abubakkar <abubakkarmit@gmail.com>
2022-10-05 18:46:59 +0500

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Footer =() => {
-    return (
-        <>
-        <footer className=" fixed-bottom bg-footer text-center">
-            <p>copyright © 2022 PayMe. All Rights Reserved | Terms and Conditions </p>
-        </footer>
-        </>
-    )
-}
+const Footer = () => {
+  return (
+    <>
+      <footer className=" fixed-bottom bg-footer text-center">
+        <p>
+          copyright © 2022 PayMe. All Rights Reserved | Terms and Conditions{" "}
+        </p>
+      </footer>
+    </>
+  );
+};
 export default Footer;
